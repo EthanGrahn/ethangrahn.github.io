e12d09a76454f2f134f536d99d6dd64e3e88df25
Ethan Grahn <egrahn@usgs.gov>
2022-06-22 20:54:50 +0000

    Configure page title

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,11 @@ module.exports = {
   outputDir: path.resolve(__dirname, './docs'),
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Portfolio - Ethan Grahn'
+    }
+  }
 }
